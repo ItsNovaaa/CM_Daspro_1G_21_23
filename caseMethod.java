e558623a72e50ea90456fa6b1c4bba0a0aa740d4
Nova <ininopal222@gmail.com>
2024-12-06 13:52:37 +0700

    Init Java 23

---
+++ caseMethod.java
@@ -0,0 +1,13 @@
+package CM_Daspro_1G_21_3;
+
+import java.util.Scanner;
+
+public class caseMethod {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+            System.out.println("===== Menu UTAMA =====");
+            System.out.println("1. Tambah Data");      
+        }
+	}
+}
